b2243f20d1d6f8855f034293434646e2dd9aecdf
ShadySentry <eurohawk@gmail.com>
2021-06-01 11:36:46 +0300

    wsdl is valid

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -16,6 +16,6 @@ public class MailServiceClient {
 
         MailService mailService = service.getPort(MailService.class);
         mailService.sendToGroup(ImmutableSet.of(
-                new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+                new Addressee("eurohawk@gmail.com", null)), null, "Subject", "Body");
     }
 }
