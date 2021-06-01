519687d4c30a75b99eb6a2b47e71fedc27f37c41
ShadySentry <eurohawk@gmail.com>
2021-06-01 16:20:02 +0300

    8.5

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
@@ -6,6 +6,8 @@ import ru.javaops.masterjava.web.WsClient;
 
 import javax.xml.namespace.QName;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Slf4j
 public class MailWSClient {
